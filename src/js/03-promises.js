c5aca541dc8d29fe15c8f0b9f929efdeca2a82a5
Stanishovskyi <arturstepanovi4@gmail.com>
2024-02-17 14:13:26 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,26 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({
+          position,
+          delay,
+        });
+      } else {
+        reject({
+          position,
+          delay,
+        });
+      }
+    }, delay);
+  });
+}
+
 function bindEvents() {
   const form = document.querySelector('.form');
 
